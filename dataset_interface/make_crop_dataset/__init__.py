5ec0efa468fe93b2a41b31c5ddfa3027ea5f8af3
1Wata <1715216037@qq.com>
2025-04-27 13:29:22 +0800

    Update .gitignore and tracked files

--- dataset_interface/make_crop_dataset/__init__.py
+++
@@ -1,7 +0,0 @@
-from .utils import jitter_bbox, crop_and_pad_template, crop_and_pad_search,\
-    convert_bbox_from_cropped_img, convert_bbox_into_cropped_img, convert_bbox_format
-
-
-__all__ = ['jitter_bbox, crop_and_pad_template, crop_and_pad_search',
-           'convert_bbox_from_cropped_img', 'convert_bbox_into_cropped_img',
-           'convert_bbox_format']
